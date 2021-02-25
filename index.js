e0d5a6e6fd895a675d96bbdc2536bec42cb72edc
Gustavo Faria <gustavo@pop-os.localdomain>
2021-02-24 22:46:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// const {} = require(
+//     '@joaoderocha/redis-pub-sub-nodejs/headless/redis/services/pub-sub',
+// );
+// const {} = require('@joaoderocha/redis-pub-sub-nodejs/headless/utils');
+
+// const reducer =(channel, message) => {};
