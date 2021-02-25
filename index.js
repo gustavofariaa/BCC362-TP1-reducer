9a76ad94510ae568c4d57200648667c2908762c6
joaoderocha <jharocha@gmail.com>
2021-02-25 01:51:38 -0300

    fix: reducer

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-'use strict';
+"use strict";
 
-// const {} = require(
-//     '@joaoderocha/redis-pub-sub-nodejs/headless/redis/services/pub-sub',
-// );
-// const {} = require('@joaoderocha/redis-pub-sub-nodejs/headless/utils');
+const {
+  DbReduce,
+  lockDB,
+  unlockDB,
+  connect,
+} = require("@joaoderocha/redis-pub-sub-nodejs/headless/database");
 
-// const reducer =(channel, message) => {};
+const {
+  roundRobinSubscribe,
+  REDUCE,
+} = require("@joaoderocha/redis-pub-sub-nodejs/headless/utils");
+
+const somaMapas = (mapa1, mapa2) => {
+  return Object.entries(mapa1).reduce((acc, [key, value]) => {
+    if (acc[key]) {
+      acc[key] += value;
+    } else {
+      acc[key] = value;
+    }
+
+    return acc;
+  }, mapa2);
+};
+
+const reducer = async (channel, message) => {
+  let numeroDeDocumentos = await DbReduce.total();
+  if (numeroDeDocumentos === 0) {
+    return DbReduce.cadastrar({ canal: fila, message });
+  }
+  await lockDB();
+  numeroDeDocumentos = await DbReduce.total();
+  if (numeroDeDocumentos === 0) {
+    await unlockDB();
+    return DbReduce.cadastrar({ canal: fila, message });
+  }
+  const { message: obj } = await DbReduce.findOneAndRemove({});
+
+  await unlockDB();
+
+  const { linha } = message;
+
+  console.log(DbReduce.cadastrar(somaMapas(linha, obj)));
+};
+
+connect();
+
+roundRobinSubscribe(REDUCE, reducer);
